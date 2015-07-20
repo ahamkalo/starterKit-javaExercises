911be45c95eff78707e314c2e66b384ccb0e6dee
Aleksander Hamkalo <aleksander.hamkalo@capgemini.com>
2015-07-20 12:46:59 +0200

    set position test added

--- src/test/java/com/capgemini/taxi/ClientTest.java
+++ src/test/java/com/capgemini/taxi/ClientTest.java
@@ -12,5 +12,13 @@ public class ClientTest {
 		assertEquals(1, client.getPosition().getXPosition());
 		assertEquals(3, client.getPosition().getYPosition());
 	}
+	@Test
+	public void shouldProperlySetPosition() {
+		FemaleClient client = new FemaleClient(new Position(1,3),"Jan");
+		client.setPosition(new Position(2,4));
+		assertEquals(2, client.getPosition().getXPosition());
+		assertEquals(4, client.getPosition().getYPosition());
+	}
+
 
 }
