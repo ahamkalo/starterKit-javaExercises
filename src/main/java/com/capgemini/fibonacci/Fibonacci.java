f8f4b1096943f3e25479c1169c2b33c8668169cb
Aleksander Hamkalo <aleksander.hamkalo@capgemini.com>
2015-07-22 08:31:15 +0200

    commit test

--- src/main/java/com/capgemini/fibonacci/Fibonacci.java
+++ src/main/java/com/capgemini/fibonacci/Fibonacci.java
@@ -5,7 +5,6 @@ package com.capgemini.fibonacci;
  * F14 F15 F16 F17 F18 F19 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597
  * 2584 4181
  */
-
 public class Fibonacci {
 	public static long fib(int n) {
 		if (n < 1) {
